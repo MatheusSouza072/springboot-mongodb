314c4df8bef8e24d76f93e2e7928cde6bc2e2181
Matheuskbrasil <matheus.070202@gmail.com>
2019-07-17 14:05:30 -0300

    Add Insert

--- src/main/java/com/matheus/springbootmng/services/UserService.java
+++ src/main/java/com/matheus/springbootmng/services/UserService.java
@@ -1,10 +1,14 @@
 package com.matheus.springbootmng.services;
 
+import java.net.URI;
 import java.util.List;
 
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.ResponseEntity;
-import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestMethod;
+import org.springframework.web.servlet.support.ServletUriComponentsBuilder;
 
 import com.matheus.springbootmng.domain.User;
 import com.matheus.springbootmng.dto.UserDTO;
@@ -14,10 +18,11 @@ import com.matheus.springbootmng.services.exception.ObjectNotFoundException;
 public class UserService {
 	@Autowired
 	private UserRepository repo;
-	
+
 	public List<User> findAll() {
 		return repo.findAll();
 	}
+
 	public User findById(String id) {
 		User user = repo.findOne(id);
 		if (user == null) {
@@ -25,6 +30,14 @@ public class UserService {
 		}
 		return user;
 	}
-	
+
+	public User insert(User obj) {
+		return repo.insert(obj);
+	}
+
+	public User fromDTO(UserDTO objDto) {
+		return new User(objDto.getId(), objDto.getName(), objDto.getEmail());
+	}
+
 	
 }
