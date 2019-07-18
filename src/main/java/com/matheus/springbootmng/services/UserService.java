0006606bf281ff870738f98b6a04ef5f7c08058a
Matheuskbrasil <matheus.070202@gmail.com>
2019-07-18 14:09:01 -0300

    User Post

--- src/main/java/com/matheus/springbootmng/services/UserService.java
+++ src/main/java/com/matheus/springbootmng/services/UserService.java
@@ -1,28 +1,25 @@
 package com.matheus.springbootmng.services;
 
-import java.net.URI;
 import java.util.List;
 
 import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.http.ResponseEntity;
-import org.springframework.web.bind.annotation.RequestBody;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RequestMethod;
-import org.springframework.web.servlet.support.ServletUriComponentsBuilder;
+import org.springframework.stereotype.Service;
 
 import com.matheus.springbootmng.domain.User;
 import com.matheus.springbootmng.dto.UserDTO;
 import com.matheus.springbootmng.repository.UserRepository;
 import com.matheus.springbootmng.services.exception.ObjectNotFoundException;
 
+@Service
 public class UserService {
+
 	@Autowired
 	private UserRepository repo;
-
+	
 	public List<User> findAll() {
 		return repo.findAll();
 	}
-
+	
 	public User findById(String id) {
 		User user = repo.findOne(id);
 		if (user == null) {
@@ -30,14 +27,28 @@ public class UserService {
 		}
 		return user;
 	}
-
+	
 	public User insert(User obj) {
 		return repo.insert(obj);
+	}
+	
+	public void delete(String id) {
+		findById(id);
+		repo.delete(id);
+	}
+	
+	public User update(User obj) {
+		User newObj = repo.findOne(obj.getId());
+		updateData(newObj, obj);
+		return repo.save(newObj);
+	}
+	
+	private void updateData(User newObj, User obj) {
+		newObj.setName(obj.getName());
+		newObj.setEmail(obj.getEmail());
 	}
 
 	public User fromDTO(UserDTO objDto) {
 		return new User(objDto.getId(), objDto.getName(), objDto.getEmail());
 	}
-
-	
 }
